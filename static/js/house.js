d6cb1393875da772f5e03b554946314162d3f325
Karol Workowski <charls7c@gmail.com>
2022-12-17 11:01:12 +0100

    first

---
+++ static/js/house.js
@@ -0,0 +1,21 @@
+function buildResult(data){
+    let details = document.getElementById('actor-details')
+    details.innerHTML = ` <p>${data["name"]}</p>  <p>${data["birthday"]}</p>`;
+}
+
+function getActorInfo(){
+    let characterDetails = document.getElementsByClassName("character_name")
+
+    for (let i = 0; i< characterDetails.length; i++){
+        characterDetails[i].addEventListener('click', ()=> {
+        return fetch(`http://127.0.0.1:5000/api/actor/`+ characterDetails[i].id)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                buildResult(data[0]);
+            })
+    })
+}
+
+}
+getActorInfo()
